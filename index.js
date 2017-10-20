df80644ea71834fe2c006ef8ff29fce1c78bcbba
taco.chen <taco.chen@tacochende-MacBook-Pro.local>
2017-10-20 13:38:50 +0800

    add npm package file and server file

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine","ejs");
+
+app.get("/",function(req,res){
+  res.render("index");
+})
+
+app.listen(8080,() => {
+  console.log("listen on port 8080");
+})
