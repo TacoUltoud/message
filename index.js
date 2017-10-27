958d40393660f73b45df86e8559b64d90222d32e
taco.chen <taco.chen@tacochende-MacBook-Pro.local>
2017-10-27 15:22:14 +0800

    modify mocha exe path & add process.env.PORT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./router.js");
+const PORT = process.env.PORT || 8080
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,8 +12,8 @@ app.get("/",router.getHomepage)
 
 app.post("/send",router.send)
 
-app.listen(8080,() => {
-  console.log("listen on port 8080");
+app.listen(PORT,() => {
+  console.log(`listen on port ${PORT}`);
 })
 
 module.exports = app
